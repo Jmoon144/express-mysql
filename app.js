7120707def87c60fe0187fa0cd825ce5eca7edf5
박준영 <sjwm98@gmail.com>
2021-09-19 15:36:39 +0900

    Add: post 연습

--- app.js
+++ app.js
@@ -3,3 +3,17 @@ var app = express()
 app.listen(3000, function(){
     console.log('start expree server on port 3000');
 });
+
+app.use(express.static('public'))
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/public/main.html');
+});
+
+app.get('/main', function(req, res){
+    res.sendFile(__dirname + '/public/main.html');
+});
+
+app.post('/email_post', function(req, res){
+    res.send('post response')
+})
